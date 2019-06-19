1bad6f1e783a8bae4b073698bdefdecfe3718b9c
Doug Swisher <big.swish@gmail.com>
2019-06-18 19:48:01 -0500

    Week1: Part 1 - drawing the @ and moving it around

---
+++ README.md
@@ -0,0 +1,11 @@
+# csrogue
+
+Roguelike using dotnet core
+
+# Links
+
+* RoguelikeDev Tutorial 2019 - [Week1](https://www.reddit.com/r/roguelikedev/comments/c1xj5b/roguelikedev_does_the_complete_roguelike_tutorial/)
+* [System.Console](https://docs.microsoft.com/en-us/dotnet/api/system.console?view=netcore-2.2)
+* [RogueBasin](http://www.roguebasin.com/index.php?title=C_Sharp) C# page
+* Libs: [CsConsoleFormat](https://github.com/Athari/CsConsoleFormat) - [Colorful.Console](https://github.com/tomakita/Colorful.Console)
+
---
+++ src/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace csrogue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = Console.BufferHeight;
+            int width = Console.BufferWidth;
+
+            // TODO - how to restore the console when we're done?
+            Console.Clear();
+            Console.CursorVisible = false;
+
+            try
+            {
+                int x = width / 2;
+                int y = height / 2;
+
+                bool done = false;
+                while (!done)
+                {
+                    // Draw character
+                    Console.SetCursorPosition(x, y);
+                    Console.Write('@');
+
+                    // Get a key
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                    bool redraw = true;
+                    int lastX = x;
+                    int lastY = y;
+                    // TODO - add a point class to lump X and Y together!
+                    switch (keyInfo.Key)
+                    {
+                        case ConsoleKey.Escape:
+                            done = true;
+                            redraw = false;
+                            break;
+
+                        case ConsoleKey.H:
+                        case ConsoleKey.LeftArrow:
+                            if (x > 0)
+                            {
+                                x -= 1;
+                            }
+                            break;
+
+                        case ConsoleKey.J:
+                        case ConsoleKey.DownArrow:
+                            if (y < height - 1)
+                            {
+                                y += 1;
+                            }
+                            break;
+
+                        case ConsoleKey.K:
+                        case ConsoleKey.UpArrow:
+                            if (y > 0)
+                            {
+                                y -= 1;
+                            }
+                            break;
+
+                        case ConsoleKey.L:
+                        case ConsoleKey.RightArrow:
+                            if (x < width - 1)
+                            {
+                                x += 1;
+                            }
+                            break;
+                    }
+
+                    if (redraw)
+                    {
+                        Console.SetCursorPosition(lastX, lastY);
+                        Console.Write(' ');
+                    }
+                }
+            }
+            finally
+            {
+                Console.CursorVisible = true;   // TODO - put this in a finally block!
+                Console.SetCursorPosition(0, height - 1);
+            }
+
+            Console.WriteLine("Game Over.");
+        }
+    }
+}
