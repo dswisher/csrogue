aa6bb405bf2fb98f557e083053f29c0255b9167c
Doug Swisher <big.swish@gmail.com>
2019-06-21 07:29:01 -0500

    Set up as a solution

---
+++ src/game/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace csrogue
+{
+    public class Entity
+    {
+        private static int nextId = 1;
+
+        public string Id { get; private set; }
+
+        public Entity()
+        {
+            Id = (nextId++).ToString();
+        }
+    }
+}
---
+++ src/game/EntityManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csrogue
+{
+    public class EntityManager
+    {
+        private List<Entity> entities = new List<Entity>();
+
+        public EntityManager()
+        {
+        }
+
+        public Entity CreateEntity()
+        {
+            Entity entity = new Entity();
+
+            entities.Add(entity);
+
+            return entity;
+        }
+    }
+}
